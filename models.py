2e3b76bfca3dd6d6483cb12850b757ecca58fb94
joezuntz <joezuntz@googlemail.com>
2022-05-06 10:09:28 -0700

    initial version

---
+++ models.py
@@ -0,0 +1,76 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from .vae import save_model, Sampling
+
+# Model with padding
+
+def make_model_1_padding(verbose=False):
+    latent_dim = 2
+    encoder_inputs = keras.Input(shape=(6, 79))
+    x = tf.keras.layers.Reshape((6, 79, 1))(encoder_inputs)
+    x = tf.keras.layers.ZeroPadding2D(padding=(3,0), data_format='channels_last')(x)
+    x = tf.keras.layers.Conv2D(64, (3, 21), activation='relu', padding='valid', data_format='channels_last')(x)
+    x = tf.keras.layers.Conv2D(32, (3, 21), activation='relu', padding='valid', data_format='channels_last')(x)
+    x = tf.keras.layers.Flatten()(x)
+    x = layers.Dense(16, activation="relu")(x)
+    z_mean = layers.Dense(latent_dim, name="z_mean")(x)
+    z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
+    z = Sampling()([z_mean, z_log_var])
+    encoder = keras.Model(encoder_inputs, [z_mean, z_log_var, z], name="encoder")
+
+    latent_inputs = keras.Input(shape=(latent_dim,))
+    x = layers.Dense(12 * 79, activation="relu")(latent_inputs)
+    x = layers.Reshape((12, 79, 1))(x)
+    x = layers.Conv2DTranspose(32, 3, activation="relu", strides=1, padding="same")(x)
+    x = layers.Conv2DTranspose(64, 3, activation="relu", strides=1, padding="same")(x)
+    x = layers.Conv2DTranspose(1, 3, activation="relu", strides=1, padding="same")(x)
+    x = tf.keras.layers.CenterCrop(6, 79)(x)
+    x = tf.keras.layers.Reshape((6, 79))(x)
+    decoder_outputs = x
+
+    decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
+    save_model('models/conv1-padding', encoder, decoder, latent_dim)
+
+    if verbose:
+        encoder.summary()
+        decoder.summary()
+    
+# Model with padding
+def make_model_1_no_padding(verbose=False):
+    latent_dim = 2
+    encoder_inputs = keras.Input(shape=(6, 79))
+    x = tf.keras.layers.Reshape((6, 79, 1))(encoder_inputs)
+    x = tf.keras.layers.Conv2D(64, (3, 21), activation='relu', padding='valid', data_format='channels_last')(x)
+    x = tf.keras.layers.Conv2D(32, (3, 21), activation='relu', padding='valid', data_format='channels_last')(x)
+    x = tf.keras.layers.Flatten()(x)
+    x = layers.Dense(16, activation="relu")(x)
+    z_mean = layers.Dense(latent_dim, name="z_mean")(x)
+    z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
+    z = Sampling()([z_mean, z_log_var])
+    encoder = keras.Model(encoder_inputs, [z_mean, z_log_var, z], name="encoder")
+    encoder.summary()
+
+    latent_inputs = keras.Input(shape=(latent_dim,))
+    x = layers.Dense(6 * 79, activation="relu")(latent_inputs)
+    x = layers.Reshape((6, 79, 1))(x)
+    x = layers.Conv2DTranspose(32, 3, activation="relu", strides=1, padding="same")(x)
+    x = layers.Conv2DTranspose(64, 3, activation="relu", strides=1, padding="same")(x)
+    x = layers.Conv2DTranspose(1, 3, activation="relu", strides=1, padding="same")(x)
+    x = tf.keras.layers.Reshape((6, 79))(x)
+    decoder_outputs = x
+
+    decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
+    decoder.summary()
+    save_model('models/conv1-no-padding', encoder, decoder, latent_dim)
+    if verbose:
+        encoder.summary()
+        decoder.summary()
+
+    
+    
+    
+def make_all_models(verbose=False):
+    make_model_1_no_padding(verbose=verbose)
+    make_model_1_padded(verbose=verbose)
---
+++ vae.py
@@ -0,0 +1,127 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import numpy as np
+import json
+import matplotlib.pyplot as plt
+
+# The new kind of layer that we need for sampling
+class Sampling(layers.Layer):
+    """
+    This is a new layer type that is added to the 
+    Uses (z_mean, z_log_var) to sample z, the vector encoding a digit.
+    """
+
+    def call(self, inputs):
+        z_mean, z_log_var = inputs
+        batch = tf.shape(z_mean)[0]
+        dim = tf.shape(z_mean)[1]
+        epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
+        return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+
+    
+def save_model(filename, encoder, decoder, latent_dim):
+    with open(filename + '.encoder.json', 'w') as f:
+        f.write(encoder.to_json())
+    with open(filename + '.decoder.json', 'w') as f:
+        f.write(decoder.to_json())
+    with open(filename + '.info.json', 'w') as f:
+        json.dump({'latent_dim':latent_dim}, f)
+
+def load_model(filename):
+    with open(filename + '.encoder.json', 'r') as f:
+        encoder = keras.models.model_from_json(f.read(), custom_objects={'Sampling':Sampling})
+    with open(filename + '.decoder.json', 'r') as f:
+        decoder = keras.models.model_from_json(f.read())
+    with open(filename + '.info.json', 'r') as f:
+        d = json.load(f)
+        latent_dim = d['latent_dim']
+    return encoder, decoder, latent_dim
+
+def load_data():
+    nz_data = np.loadtxt("data/realizations.txt.gz").reshape((2400, 6, 79))
+    nz_data /= nz_data.max()
+    return nz_data
+
+class VAE(keras.Model):
+    def __init__(self, encoder, decoder, latent_dim,  **kwargs):
+        super(VAE, self).__init__(**kwargs)
+        self.encoder = encoder
+        self.decoder = decoder
+        self.latent_dim = latent_dim
+        self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
+        self.reconstruction_loss_tracker = keras.metrics.Mean(
+            name="reconstruction_loss"
+        )
+        self.kl_loss_tracker = keras.metrics.Mean(name="kl_loss")
+
+    @property
+    def metrics(self):
+        return [
+            self.total_loss_tracker,
+            self.reconstruction_loss_tracker,
+            self.kl_loss_tracker,
+        ]
+    
+    def normalize_nz(self, nz):
+        return tf.linalg.normalize(nz, axis=(2))[0]
+
+    def train_step(self, data):
+        data = self.normalize_nz(data)
+        with tf.GradientTape() as tape:
+            # encode and decode again
+            z_mean, z_log_var, z = self.encoder(data)
+            reconstruction = self.decoder(z)
+            reconstruction = self.normalize_nz(reconstruction)
+            
+            # loss value from how different our reconstruction
+            # looks from the data
+            reconstruction_loss = tf.reduce_mean(
+                tf.reduce_sum(
+                    keras.losses.binary_crossentropy(data, reconstruction, axis=(1, 2))
+                )
+            )
+            
+            # loss from how different the z values are from a normal distribution
+            kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
+            kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
+            
+            # Total is the sum of these two
+            total_loss = reconstruction_loss + kl_loss
+
+        # do the training step update
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+        return {
+            "loss": self.total_loss_tracker.result(),
+            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+            "kl_loss": self.kl_loss_tracker.result(),
+        }
+    
+    def generate_nz_draws(self, n):
+        z_sample = np.random.normal(size=(n, self.latent_dim))
+        nz_generated = self.decoder.predict(z_sample)
+
+        return self.normalize_nz(nz_generated)
+    
+    def fit_to_nz_data(self, nz_data, epochs, batch_size=2400):
+        self.compile(optimizer=keras.optimizers.Adam())
+        history = self.fit(nz_data, epochs=epochs, batch_size=batch_size, verbose=0)        
+        return history
+
+    def plot_history(self, history):
+        plt.figure()
+        plt.plot(history.history['kl_loss'])
+        plt.xlabel("Epoch")
+        plt.ylabel("KL Loss")
+        plt.figure()
+        plt.plot(history.history['reconstruction_loss'])
+        plt.xlabel("Epoch")
+        plt.ylabel("Reconstruction Loss")
+        plt.figure()
+        plt.plot(history.history['loss'])
+        plt.xlabel("Epoch")
+        plt.ylabel("Total Loss")        
