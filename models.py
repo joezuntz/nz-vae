db5a3507a48e4eb8a4776e514bb4deb69444b8e6
joezuntz <joezuntz@googlemail.com>
2022-05-09 09:15:55 -0700

    nicer models

--- models.py
+++ models.py
@@ -50,7 +50,6 @@ def make_model_1_no_padding(verbose=False):
     z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
     z = Sampling()([z_mean, z_log_var])
     encoder = keras.Model(encoder_inputs, [z_mean, z_log_var, z], name="encoder")
-    encoder.summary()
 
     latent_inputs = keras.Input(shape=(latent_dim,))
     x = layers.Dense(6 * 79, activation="relu")(latent_inputs)
@@ -62,8 +61,76 @@ def make_model_1_no_padding(verbose=False):
     decoder_outputs = x
 
     decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
-    decoder.summary()
     save_model('models/conv1-no-padding', encoder, decoder, latent_dim)
+    if verbose:
+        encoder.summary()
+        decoder.summary()
+
+def make_dense_model_1(verbose=False):
+    encoder_inputs = keras.Input(shape=(6, 79))
+    x = tf.keras.layers.Flatten()(encoder_inputs)
+    x = tf.keras.layers.Dense(64, activation='relu')(x)
+    x = tf.keras.layers.Dense(64, activation='relu')(x)
+    x = layers.Dense(16, activation="relu")(x)
+    z_mean = layers.Dense(latent_dim, name="z_mean")(x)
+    z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
+    z = Sampling()([z_mean, z_log_var])
+    encoder = keras.Model(encoder_inputs, [z_mean, z_log_var, z], name="encoder")
+
+    latent_inputs = keras.Input(shape=(latent_dim,))
+    x = tf.keras.layers.Dense(64, activation='relu')(latent_inputs)
+    x = tf.keras.layers.Dense(6*79, activation='relu')(x)
+    x = tf.keras.layers.Reshape((6,79))(x)
+    decoder_outputs = x
+
+    decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
+    save_model('models/dense-1', encoder, decoder, latent_dim)
+    if verbose:
+        encoder.summary()
+        decoder.summary()
+
+def make_dense_model_2(latent_dim = 2, verbose=False):
+    encoder_inputs = keras.Input(shape=(6, 79))
+    x = tf.keras.layers.Flatten()(encoder_inputs)
+    x = tf.keras.layers.Dense(64, activation='relu')(x)
+    x = tf.keras.layers.Dense(64, activation='relu')(x)
+    x = layers.Dense(16, activation="relu")(x)
+    z_mean = layers.Dense(latent_dim, name="z_mean")(x)
+    z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
+    z = Sampling()([z_mean, z_log_var])
+    encoder = keras.Model(encoder_inputs, [z_mean, z_log_var, z], name="encoder")
+
+    latent_inputs = keras.Input(shape=(latent_dim,))
+    x = tf.keras.layers.Dense(64, activation='relu')(latent_inputs)
+    x = tf.keras.layers.Dense(6*79, activation='relu')(x)
+    x = tf.keras.layers.Reshape((6,79))(x)
+    decoder_outputs = x
+
+    decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
+    save_model('models/dense-2', encoder, decoder, latent_dim)
+    if verbose:
+        encoder.summary()
+        decoder.summary()
+
+def make_dense_model_3(latent_dim, verbose=False):
+    encoder_inputs = keras.Input(shape=(6, 79))
+    x = tf.keras.layers.Flatten()(encoder_inputs)
+    x = tf.keras.layers.Dense(128, activation='relu')(x)
+    x = tf.keras.layers.Dense(128, activation='relu')(x)
+    x = layers.Dense(16, activation="relu")(x)
+    z_mean = layers.Dense(latent_dim, name="z_mean")(x)
+    z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
+    z = Sampling()([z_mean, z_log_var])
+    encoder = keras.Model(encoder_inputs, [z_mean, z_log_var, z], name="encoder")
+
+    latent_inputs = keras.Input(shape=(latent_dim,))
+    x = tf.keras.layers.Dense(128, activation='relu')(latent_inputs)
+    x = tf.keras.layers.Dense(6*79, activation='relu')(x)
+    x = tf.keras.layers.Reshape((6,79))(x)
+    decoder_outputs = x
+
+    decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
+    save_model('models/dense-3', encoder, decoder, latent_dim)
     if verbose:
         encoder.summary()
         decoder.summary()
--- vae.py
+++ vae.py
@@ -39,15 +39,16 @@ def load_model(filename):
     return encoder, decoder, latent_dim
 
 def load_data():
-    nz_data = np.loadtxt("data/realizations.txt.gz").reshape((2400, 6, 79))
-    nz_data /= nz_data.max()
+    nz_data = np.loadtxt("data/realizations.txt.gz").reshape((2400, 6, 79)).astype(np.float32)
+    return VAE.normalize_nz(nz_data)
     return nz_data
 
 class VAE(keras.Model):
-    def __init__(self, encoder, decoder, latent_dim,  **kwargs):
+    def __init__(self, encoder, decoder, latent_dim,  kl_weight=1, **kwargs):
         super(VAE, self).__init__(**kwargs)
         self.encoder = encoder
         self.decoder = decoder
+        self.kl_weight = kl_weight
         self.latent_dim = latent_dim
         self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
         self.reconstruction_loss_tracker = keras.metrics.Mean(
@@ -63,7 +64,8 @@ class VAE(keras.Model):
             self.kl_loss_tracker,
         ]
     
-    def normalize_nz(self, nz):
+    @staticmethod
+    def normalize_nz(nz):
         return tf.linalg.normalize(nz, axis=(2))[0]
 
     def train_step(self, data):
@@ -83,8 +85,8 @@ class VAE(keras.Model):
             )
             
             # loss from how different the z values are from a normal distribution
-            kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
-            kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
+            kl_loss1 = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
+            kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss1, axis=1))  * self.kl_weight
             
             # Total is the sum of these two
             total_loss = reconstruction_loss + kl_loss
@@ -112,16 +114,49 @@ class VAE(keras.Model):
         history = self.fit(nz_data, epochs=epochs, batch_size=batch_size, verbose=0)        
         return history
 
-    def plot_history(self, history):
+    def plot_history(self, history, log=False):
+        plotter = plt.semilogy if log else plt.plot
         plt.figure()
-        plt.plot(history.history['kl_loss'])
+        plotter(history.history['kl_loss'])
         plt.xlabel("Epoch")
         plt.ylabel("KL Loss")
         plt.figure()
-        plt.plot(history.history['reconstruction_loss'])
+        plotter(history.history['reconstruction_loss'])
         plt.xlabel("Epoch")
         plt.ylabel("Reconstruction Loss")
         plt.figure()
-        plt.plot(history.history['loss'])
+        plotter(history.history['loss'])
         plt.xlabel("Epoch")
         plt.ylabel("Total Loss")        
+    
+    
+    
+class VAE2(VAE):
+    def train_step(self, data):
+        data = self.normalize_nz(data)
+        with tf.GradientTape() as tape:
+            # encode and decode again
+            z_mean, z_log_var, z = self.encoder(data)
+            reconstruction = self.decoder(z)
+            reconstruction = self.normalize_nz(reconstruction)
+            
+            reconstruction_loss = tf.reduce_sum(tf.square(tf.subtract(reconstruction, data)))
+
+            # loss from how different the z values are from a normal distribution
+            kl_loss1 = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
+            kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss1, axis=1))
+            
+            # Total is the sum of these two
+            total_loss = reconstruction_loss + kl_loss
+
+        # do the training step update
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+        return {
+            "loss": self.total_loss_tracker.result(),
+            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+            "kl_loss": self.kl_loss_tracker.result(),
+        }
